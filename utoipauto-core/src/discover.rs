74e4ed1a1c7d8fbc9600377085d3c5e03e6af2a3
Matthew Gapp <61894094+matthewgapp@users.noreply.github.com>
2024-06-09 21:34:27 -0700

    wip

--- utoipauto-core/src/discover.rs
+++ utoipauto-core/src/discover.rs
@@ -14,15 +14,15 @@ pub fn discover_from_file(
     crate_name: String,
 ) -> (Vec<String>, Vec<String>, Vec<String>) {
     // current working directory is
-    let root = std::env::current_dir().unwrap();
+    let root = std::env::var("WORKSPACE_ROOT").unwrap();
 
     let src_path = Path::new(&src_path);
-    // if src_path and full_path start and end with the same dirctory, then omit one
-    let src_path = if src_path.starts_with(&root.iter().last().unwrap()) {
-        src_path.strip_prefix(&root.iter().last().unwrap()).unwrap()
-    } else {
-        src_path
-    };
+    // // if src_path and full_path start and end with the same dirctory, then omit one
+    // let src_path = if src_path.starts_with(&root.iter().last().unwrap()) {
+    //     src_path.strip_prefix(&root.iter().last().unwrap()).unwrap()
+    // } else {
+    //     src_path
+    // };
 
     let full_path = path::Path::new(&root).join(&src_path);
     // current directory is
